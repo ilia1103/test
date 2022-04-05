d4fffb0cfb5c6c09ac5b69f2e35d1f491a72f7b4
ilia1103 <ylia-gorbachev@tonar.net>
2022-04-05 14:08:39 +0300

    second commit

---
+++ app/src/main/java/com/example/tonar_robots/FileNotFound.java
@@ -0,0 +1,14 @@
+package com.example.tonar_robots;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class FileNotFound extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_file_not_found);
+    }
+}
